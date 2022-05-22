2e0c6a9c48fbf9fe9bbaf31fcb42bb32c03254ee
Dmitry Kuchukov <kudim@inbox.ru>
2022-05-22 15:07:40 +0300

    delete semi-comma

--- src/main/java/plaindoll/HelloPlayer.java
+++ src/main/java/plaindoll/HelloPlayer.java
@@ -4,6 +4,6 @@ public class HelloPlayer{
 	public static void main(String[] args) {
 		Welcomer welcomer = new Welcomer();
 		System.out.println(welcomer.sayWelcome());
-		System.out.println(welcomer.sayFarewell());
+		System.out.println(welcomer.sayFarewell())
 	}
 }
